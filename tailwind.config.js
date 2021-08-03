e909b1f6c5f86d3994103bbc962ba2b085e450db
Mark Marilag <mark.marilag27@gmail.com>
2021-08-04 00:40:07 +0800

    [General] Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
